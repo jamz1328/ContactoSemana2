aaa2189fbdbe14099ad5502a6b61300430345f77
jamz1328 <jamz132842@gmail.com>
2025-01-27 20:30:54 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContactoSemana2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
